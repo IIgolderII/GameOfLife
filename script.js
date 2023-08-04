405bb0c374dd2f5bf984f61ef73d15b54ce70a9a
Antoine FAVEREAU <antoine.favereau@viacesi.fr>
2023-08-05 01:18:29 +0200

    choix des models

--- script.js
+++ script.js
@@ -6,26 +6,151 @@ var ctx = canvas.getContext("2d");
 let arrayLength = 20;
 let tab = Array.from({ length: arrayLength }, () =>
     Array.from({ length: arrayLength }, () => 0)
-); // full of false
-tab[9][9] = true;
-tab[10][9] = true;
-tab[9][10] = true;
-tab[8][9] = true;
-tab[9][8] = true;
+); // full of 0
+
 const tileWidth = canvas.height / arrayLength;
 
+let game;
+
+const models = {
+    "Block": [
+        [1, 1],
+        [1, 1],
+    ],
+    "Bee-hive": [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0],
+    ],
+    "Loaf": [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 0, 1],
+        [0, 0, 1, 0],
+    ],
+    "Boat": [
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0],
+    ],
+    "Tub": [
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0],
+    ],
+    "Blinker": [
+        [1, 1, 1],
+    ],
+    "Toad": [
+        [0, 1, 1, 1],
+        [1, 1, 1, 0],
+    ],
+    "Beacon": [
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+    ],
+    "Pulsar": [
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    ],
+    "Penta-decathlon": [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 1, 1],
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 1],
+    ],
+    "Glider": [
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 1, 1],
+    ],
+    "Light-weight spaceship": [
+        [1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1],
+    ],
+    "Middle-weight spaceship": [
+        [0, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0],
+    ],
+    "Heavy-weight spaceship": [
+        [0, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 0, 0],
+    ],
+}
+
+document.querySelector('#models').addEventListener('click', () => {
+    document.querySelector('#models').classList.toggle("active");
+});
+
+Array.from(document.querySelectorAll('#models .list .item')).forEach(element => {
+    element.addEventListener('click', () => {
+        clearInterval(game);
+
+        if (!models[element.dataset.name]) {
+            return;
+        }
+
+        // document.querySelector('#models').classList.remove("active");
+
+        let model = models[element.dataset.name];
+
+        let topOffset = Math.floor(arrayLength / 2 - model.length / 2);
+        let leftOffset = Math.floor(arrayLength / 2 - model[0].length / 2);
+
+        tab = Array.from({ length: arrayLength }, () =>
+            Array.from({ length: arrayLength }, () => 0)
+        ); // full of false
+
+        for (let i = 0; i < model.length; i++) {
+            for (let j = 0; j < model[i].length; j++) {
+                tab[i + topOffset][j + leftOffset] = model[i][j];
+            }
+        }
+
+        draw();
+
+        game = setInterval(() => {
+            update();
+            draw();
+        }, 500);
+    });
+})
 
 draw();
 
-setInterval(() => {
-    update();
-    draw();
-}, 1000);
-
-document.addEventListener('click', () => {
-    update();
-    draw();
-});
+// document.addEventListener('click', () => {
+//     update();
+//     draw();
+// });
 
 function update() {
     let newTab = Array.from({ length: arrayLength }, () =>
